57dd9a92f5523bb09acf9fb1f8fc4beaaa178a83
main <main@grafit.lan>
2026-05-15 18:49:30 +0200

    example data

--- PJATK-APBD-Cw7-s32101/Models/ComputerMgmtDbContext.cs
+++ PJATK-APBD-Cw7-s32101/Models/ComputerMgmtDbContext.cs
@@ -45,6 +45,26 @@ public class ComputerMgmtDbContext(DbContextOptions<ComputerMgmtDbContext> optio
             entity.HasMany(s => s.Components)
                 .WithOne(s => s.ComponentType)
                 .HasForeignKey(s => s.ComponentTypesId);
+            
+            entity.HasData(
+                new ComponentType()
+                {
+                    Id = 1,
+                    Abbreviation = "MC",
+                    Name = "Memory Card"
+                },
+                new ComponentType()
+                {
+                    Id = 2,
+                    Abbreviation = "GC",
+                    Name = "Graphics Card"
+                },
+                new ComponentType()
+                {
+                    Id = 3,
+                    Abbreviation = "PCS",
+                    Name = "Processor"
+                });
         });
         
         modelBuilder.Entity<ComponentManufacturer>(entity =>
@@ -63,6 +83,29 @@ public class ComputerMgmtDbContext(DbContextOptions<ComputerMgmtDbContext> optio
             entity.HasMany(s => s.Components)
                 .WithOne(s => s.ComponentManufacturer)
                 .HasForeignKey(s => s.ComponentManufacturersId);
+            
+            entity.HasData(
+                new ComponentManufacturer(){
+                    Id = 1,
+                    FullName = "Super Computers",
+                    Abbreviation = "SP",
+                    FoundationDate = new DateTime(2000, 1, 1),
+                    
+                },
+                new ComponentManufacturer()
+                {
+                    Id = 1,
+                    FullName = "Super Bad Computers",
+                    Abbreviation = "SBP",
+                    FoundationDate = new DateTime(1995, 2, 1),
+                },
+                new ComponentManufacturer()
+                {
+                    Id = 1,
+                    FullName = "Great Manufacturer",
+                    Abbreviation = "GM",
+                    FoundationDate = new DateTime(2005, 3, 1),
+                });
         });
         
         modelBuilder.Entity<Component>(entity =>
@@ -82,6 +125,23 @@ public class ComputerMgmtDbContext(DbContextOptions<ComputerMgmtDbContext> optio
             entity.HasMany(s => s.PCComponents)
                 .WithOne(s => s.Component)
                 .HasForeignKey(s => s.ComponentCode);
+            
+            entity.HasData(
+                new Component(){
+                    ComponentTypesId = 1,
+                    ComponentManufacturersId = 1,
+                    Description = "none",
+                    Name = "Graphic Card",
+                    Code = "123",
+                },
+                new Component()
+                {
+                 
+                },
+                new Component()
+                {
+               
+                });
         });
         
         modelBuilder.Entity<PCComponent>(entity =>
@@ -90,6 +150,20 @@ public class ComputerMgmtDbContext(DbContextOptions<ComputerMgmtDbContext> optio
             entity.HasKey(s => new { s.PCId, s.ComponentCode });
             entity.Property(s => s.ComponentCode)
                 .HasColumnType("char(10)");
+
+            entity.HasData(
+                new PCComponent()
+                {
+                    
+                },
+                new PCComponent()
+                {
+
+                },
+                new PCComponent()
+                {
+
+                });
         });
         
         
