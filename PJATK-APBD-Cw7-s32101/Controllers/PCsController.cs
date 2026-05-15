a6e0905c71a07945ddafb184521e13f3f4f56a64
main <main@grafit.lan>
2026-05-15 19:19:11 +0200

    końcówka GET /api/pcs

---
+++ PJATK-APBD-Cw7-s32101/Controllers/PCsController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PJATK_APBD_Cw7_s32101.DTOs;
+using PJATK_APBD_Cw7_s32101.Models;
+
+namespace PJATK_APBD_Cw7_s32101.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PCsController(ComputerMgmtDbContext db) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var allFromDb = await db.PCs.Include(x => x.PCComponents)
+            .ThenInclude(x => x.Component)
+            .ToListAsync(); //select * from PCs - jak masz 1 milion rekordów to rip
+        
+        return Ok(allFromDb.Select(x => new PCDto()
+        {
+            Id =  x.Id,
+            Name = x.Name,
+            Weight =  x.Weight,
+            CreatedAt =  x.CreatedAt,
+            Stock =  x.Stock,
+            Warranty =   x.Warranty,
+            Components = x.PCComponents.Select(y => new PCComponentDto()
+            {
+                ComponentCode = y.ComponentCode,
+                ComponentName = y.Component.Name,
+                Amount = y.Amount
+            }).ToList()
+        }));
+    }
+}
---
+++ PJATK-APBD-Cw7-s32101/DTOs/PCComponentDto.cs
@@ -0,0 +1,10 @@
+namespace PJATK_APBD_Cw7_s32101.DTOs;
+
+public class PCComponentDto
+{
+    public string ComponentCode { get; set; } = null!;
+
+    public string ComponentName { get; set; } = null!;
+
+    public int Amount { get; set; }
+}
